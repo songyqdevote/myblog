d5b37f4999c98f7b2250cc563fcd724fd09f4c05
songyqdevote <songyqdevote@163.com>
2018-11-27 21:06:32 +0800

    使用过滤器修改前端代码

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ app_name ='blog'
 urlpatterns = [
     path('index/', views.index),
     re_path(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
-    path('edit/', views.edit_page, name='edit_page'),
+    re_path(r'^edit/(?P<article_id>[0-9]+)$', views.edit_page, name='edit_page'),
     path('edit/action/', views.edit_action, name='edit_action'),
 ]
